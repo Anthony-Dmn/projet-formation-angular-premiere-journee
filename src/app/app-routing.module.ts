a3c9a5bdd80b97ec32d436659fa92eeb3a21a916
AnthonyD <contact@anthony-demon.com>
2023-09-01 00:10:50 +0200

    Ajout vue page contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageHomeComponent } from './home/pages/page-home/page-home.component';
+import { PageContactComponent } from './contact/pages/page-contact/page-contact.component';
 
 const routes: Routes = [
   { path: '', component: PageHomeComponent },
+  { path: 'contactez-nous', component: PageContactComponent },
 ];
 
 @NgModule({
